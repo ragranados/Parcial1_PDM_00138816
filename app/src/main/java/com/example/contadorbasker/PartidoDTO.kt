6718cd1f54d930d1270016cb5faa77350a0580c1
Raul Granados <00138816@uca.edu.sv>
2019-06-02 12:21:07 -0600

    Ya esta XML de actividad de nuevo partido (sin arte) y se esta creando XML del contador

---
+++ app/src/main/java/com/example/contadorbasker/PartidoDTO.kt
@@ -0,0 +1,10 @@
+package com.example.contadorbasker
+
+class PartidoDTO(
+        val equipoUno: String= "N/A",
+        val equipoDos: String = "N/A",
+        val puntajeEquipoUno: String = "N/A",
+        val puntajeEquipoDos: String = "N/A",
+        val fecha: String = "N/A",
+        val hora: String = "N/A"
+)
---
+++ app/src/main/java/com/example/contadorbasker/activitys/Match_counter.kt
@@ -0,0 +1,13 @@
+package com.example.contadorbasker.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.contadorbasker.R
+
+class Match_counter : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_match_counter)
+    }
+}
